73b2b0ee322da9ef140cd5154dc44b4e387fcd3b
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2020-08-15 22:07:03 +0530

    completed the game by adding the particles

--- sketch.js
+++ sketch.js
@@ -6,8 +6,11 @@ const Body = Matter.Body;
 
 var world, engine;
 
+//array for storing the objects
+var particleArray = [];
+
 function setup() {
-  createCanvas(540,810);
+  createCanvas(540, 810);
 
   //creating the engine and the world
   engine = Engine.create();
@@ -22,7 +25,7 @@ function setup() {
   gndCorX = 70;//the starting x coordinate
 
   //creating the objects
-  for(var i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
     //creating all the grounds
     groundArray.push(new Ground(gndCorX, 790, 80, 20));
     gndCorX += 100;//this changes the x coordinate of the ground. 
@@ -33,17 +36,52 @@ function setup() {
   //div means a division
   divCorX = 20;//the starting x coordinate
 
-  for(i = 0; i < 6; i++) {
+  for (i = 0; i < 6; i++) {
     //creating all the divisions
     divisionArray.push(new Division(divCorX, 700));
     divCorX += 100;//this changes the x coordinate of the divisions. 
     //100 = 10+80+10 = halfDivisionWidth + ground's Width + halfdivisionWidth
   }
 
-  //arrays for storing the objects
-  pegArray = [];
-  particleArray = [];
+  drawPegs();
+}
 
+function draw() {
+  clearBackground();
+
+  Engine.update(engine);//updates the engine continuously
+
+  rectMode(CENTER);//makes all the rectangles in relation to its center
+
+  //displaying the objects
+  groundArray.forEach((item, index) => item.display());
+  divisionArray.forEach((item, index) => item.display());
+
+  pegArray.forEach((item, index) => item.display());
+
+  if (frameCount % 60 == 0) {
+    drawParticle();
+  }
+
+  particleArray.forEach((item, index) => item.display());
+}
+
+function clearBackground() {
+  //this function clears the background
+  //this is an alternative to background() function;
+  //I did this so that we have a brown border around the background
+  push();
+  rectMode(CENTER);
+  stroke("brown");
+  strokeWeight(20);
+  fill("black");
+  rect(width / 2, height / 2, 540, 810);
+  pop();
+}
+
+function drawPegs() {
+  //array for storing the objects
+  pegArray = [];
 
   //cooridnates of the pegs for using them in a loop.
   pegCorX = 50;//the starting x coordinate
@@ -52,17 +90,17 @@ function setup() {
   pegNumber = 8;//this will determine the number of pegs in a row
 
   //loop for creating all the pegs
-  for(var row = 1; row <= 10; row++){
-    for(var col = 0; col <  pegNumber; col++){
+  for (var row = 1; row <= 10; row++) {
+    for (var col = 0; col < pegNumber; col++) {
       pegArray.push(new Peg(pegCorX, pegCorY, 10));
       pegCorX += 60;
     }
 
-    if(row % 2 == 0) {
+    if (row % 2 == 0) {
       //if the row value is an even number, then this condition exucutes
       pegCorX = 50;//starts making the pegs a little before
       pegNumber = 8;
-    } 
+    }
     else {
       //if the row value is an odd number, then this condition exucutes
       pegCorX = 75;//starts making the pegs after some more space
@@ -72,33 +110,12 @@ function setup() {
 
     pegCorY += 50;//moves to another row 
   }
-}
-
-function draw() {
-  clearBackground();
-
-  Engine.update(engine);//updates the engine continuously
-
-  rectMode(CENTER);//makes all the rectangles in relation to its center
-  
-  //displaying the objects
-  groundArray.forEach((item, index) => item.display());
-  divisionArray.forEach((item, index) => item.display());
-
-  pegArray.forEach((item, index) => item.display());
-  //particle.display((item, index) => item.display());
 
 }
 
-function clearBackground(){
-  //this function clears the background
-  //this is an alternative to background() function;
-  //I did this so that we have a brown border around the background
-  push();
-  rectMode(CENTER);
-  stroke("brown");
-  strokeWeight(20);
-  fill("black");
-  rect(width/2, height/2, 540, 810);
-  pop();
-} 
+function drawParticle() {
+  particleCorX = random(20, 220);
+  particleColour = [random(0, 255), random(0, 255), random(0, 255)];
+
+  particleArray.push(new Particle(particleCorX, -20, 10, particleColour));
+}

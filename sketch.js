dd74890678d01285f8661358fbe5b96e9e2b2633
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2020-08-15 13:40:56 +0530

    fixed some problems

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
 }
 
 function draw() {
   background(255,255,255);  
-  drawSprites();
 }
